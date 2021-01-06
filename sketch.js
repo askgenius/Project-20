17cea5dc00a8ba070aebb28eaec627bd9c9649f8
askgenius <askhara84@gmail.com>
2021-01-06 22:16:20 +0530

    update

--- sketch.js
+++ sketch.js
@@ -1,22 +1,24 @@
 var wall, thikness;
-var bullets, speed, weight;
+var bullet, speed, weight;
 var damage;
 
 function setup() {
     canvas = createCanvas(1600, 400);
     wall = createprite(1200, 200, thikness, height / 2);
-    speed = random(223, 321)
-    weight = random(30, 52)
-    thikness = random(22, 83)
+    wall.shapeColour=color(80,80,80);
+    speed = random(223, 321);
+    weight = random(30, 52);
+    thikness = random(22, 83);
+    bullet.velocityX = speed;
 
 }
 
 function draw() {
-    background(0, 0, 0, 0);
+    background(0, 0, 0);
 
     if (hasCollided(bullet, wall)) {
         bullet.velocityX = 0;
-        var damage = 0.5 * weight * speed * speed / (thikness * thikness * thikness);
+        var damage = (0.5 * weight * speed * speed) / (thikness * thikness * thikness);
 
         if (damage > 10) {
             wall.shapeColour = colour(255, 0, 0);
@@ -26,8 +28,6 @@ function draw() {
             wall.shapeColour = colour(0, 255, 0);
         }
     }
-
-
     drawSprites();
 }
 
@@ -35,7 +35,7 @@ function hasCollided(bullet, lwall) {
     bulletRightEdge = lbullet.x + lbullet.width;
     wallLeftEdge = lwall.x;
     if (bulletRightEdge >= wallLeftEdge) {
-        return true
+        return true;
     }
     return false;
 }
